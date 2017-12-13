be09f152d0d06e19919c1ee55e10fb59aa026bfa
jennifer <jgonzal10@gmail.com>
2017-12-13 21:47:34 -0200

    'first'

---
+++ src/pages/tutorial/tutorial.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import { IonicPage, MenuController, NavController, Platform } from 'ionic-angular';
+
+import { TranslateService } from '@ngx-translate/core';
+
+export interface Slide {
+  title: string;
+  description: string;
+  image: string;
+}
+
+@IonicPage()
+@Component({
+  selector: 'page-tutorial',
+  templateUrl: 'tutorial.html'
+})
+export class TutorialPage {
+  slides: Slide[];
+  showSkip = true;
+  dir: string = 'ltr';
+
+  constructor(public navCtrl: NavController, public menu: MenuController, translate: TranslateService, public platform: Platform) {
+    this.dir = platform.dir();
+    translate.get(["TUTORIAL_SLIDE1_TITLE",
+      "TUTORIAL_SLIDE1_DESCRIPTION",
+      "TUTORIAL_SLIDE2_TITLE",
+      "TUTORIAL_SLIDE2_DESCRIPTION",
+      "SISTER_WAVE_START",
+      "SISTER_WAVE_START_DESCRIPTION",
+    ]).subscribe(
+      (values) => {
+        console.log('Loaded values', values);
+        this.slides = [
+          {
+            title: values.SISTER_WAVE_START,
+            description: values.SISTER_WAVE_START_DESCRIPTION,
+            image: 'assets/img/ica-slidebox-img-3.png',
+          }
+        ];
+      });
+  }
+
+  startApp() {
+    this.navCtrl.setRoot('WelcomePage', {}, {
+      animate: true,
+      direction: 'forward'
+    });
+  }
+
+  onSlideChangeStart(slider) {
+    this.showSkip = !slider.isEnd();
+  }
+
+  ionViewDidEnter() {
+    // the root left menu should be disabled on the tutorial page
+    this.menu.enable(false);
+  }
+
+  ionViewWillLeave() {
+    // enable the root left menu when leaving the tutorial page
+    this.menu.enable(true);
+  }
+
+}
