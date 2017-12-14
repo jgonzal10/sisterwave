0a8d938ecf45b08cc0234ba43c7d1f79386bed2f
jennifer <jgonzal10@gmail.com>
2017-12-14 06:24:50 -0200

    'conf'

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var path = require('path')
 var app = express();
+var morgan = require('morgan');             // log requests to the console (express4)
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var cors = require('cors');
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+// app.use(methodOverride());
+app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.static(path.resolve(__dirname, "www")));
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
